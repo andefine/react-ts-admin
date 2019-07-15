c4a5797cb471bc98d538b059ba61bc3e4b648057
andefine <1939866551@qq.com>
2019-07-15 19:40:58 +0800

    add antd and use babel-plugin-import

---
+++ src/views/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Button } from 'antd'
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Button>按钮</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/views/home/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">home</div>
+  )
+}
+
+export default Home
